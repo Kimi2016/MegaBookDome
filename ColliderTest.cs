8523156c09b8f19382297652fa2d2a7d6b0e1032
tobirohrer <tobias.rohrer@outlook.com>
2016-03-18 14:43:47 +0900

    Added files via upload

---
+++ ColliderTest.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColliderTest : MonoBehaviour {
+
+    public MegaBookBuilder book;
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("pic"))
+        {
+            Renderer renderer = other.gameObject.GetComponent<Renderer>();
+            Texture2D texture = renderer.material.GetTexture("_MainTex") as Texture2D;
+            book.SetPageTexture(texture, book.GetCurrentPage(), true);
+            other.gameObject.SetActive(false);
+            Debug.Log("collision detected"); 
+            
+        }
+    }
+}
