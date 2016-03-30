286150fb7267bf22319785b67f8791da6815dd69
Tobias Rohrer <tobi@tobipc>
2016-03-30 11:35:18 +0900

    added Motion for turn over pages, just swipe in one direction and the books pages are turning over, ps: plz dont delete the picture clone, its there for testing drag and drop stuff

--- Book/Assets/customScripts/LeapController.cs
+++ Book/Assets/customScripts/LeapController.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using Leap;
 using LeapInternal;
+using System;
 
 public class LeapController : MonoBehaviour
 {
     Leap.Controller controller;
     LeapProvider leapProvider;
     public GameObject picture;
+    public MegaBookBuilder book;
+    public AudioSource pageTurnSound;
     Vector3 picturePosition;
 
     void Start()
@@ -29,26 +32,64 @@ public class LeapController : MonoBehaviour
     {
 
         Frame frame = leapProvider.CurrentFrame;
+
         foreach (Hand hand in frame.Hands)
         {
+            
             if (hand.IsRight)
             {
-                float grabStrength = hand.GrabStrength;
-                if (grabStrength > 0.6)
-                {
-                    Debug.Log("Grab detected... strength is:" + grabStrength);
-                    Vector3 unityLeap = hand.PalmPosition.ToUnity();
-                    Vector3 distance = unityLeap - picture.transform.position;
-                    Debug.Log("distance is " + distance.magnitude);
-                    if (distance.magnitude < 0.7)
-                    {
-                        picturePosition.x = hand.PalmPosition.x;
-                        picturePosition.z = hand.PalmPosition.z;
-                        picturePosition.y = hand.PalmPosition.y - 0.2f;
-                        picture.transform.position = picturePosition;
-                    }
-                }
+                TurnOverChecker(hand);
+                
 
+            }
+        }
+    }
+
+    private void TurnOverChecker(Hand rightHand) {
+
+
+        float palmVelocityX = rightHand.PalmVelocity.x;
+        if (palmVelocityX > 4)
+        {
+            try
+            {
+                book.PrevPage(pageTurnSound);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e.GetBaseException());
+            }
+        }
+        if (palmVelocityX < -2)
+        {
+            try
+            {
+                book.NextPage(pageTurnSound);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e.GetBaseException());
+            }
+        }
+
+
+
+    }
+
+    private void DragAndDropChecker(Hand rightHand) {
+        float grabStrength = rightHand.GrabStrength;
+        if (grabStrength > 0.6)
+        {
+            //Debug.Log("Grab detected... strength is:" + grabStrength);
+            Vector3 unityLeap = rightHand.PalmPosition.ToUnity();
+            Vector3 distance = unityLeap - picture.transform.position;
+            //Debug.Log("distance is " + distance.magnitude);
+            if (distance.magnitude < 0.7)
+            {
+                picturePosition.x = rightHand.PalmPosition.x;
+                picturePosition.z = rightHand.PalmPosition.z;
+                picturePosition.y = rightHand.PalmPosition.y - 0.2f;
+                picture.transform.position = picturePosition;
             }
         }
     }
