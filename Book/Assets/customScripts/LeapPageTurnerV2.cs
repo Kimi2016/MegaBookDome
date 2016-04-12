7937f642032e5765a4658d30d970f04b9ff91015
Tobias Rohrer <tobi@tobipc>
2016-04-13 00:54:58 +0900

    updated GestureseV2

--- Book/Assets/customScripts/LeapPageTurnerV2.cs
+++ Book/Assets/customScripts/LeapPageTurnerV2.cs
@@ -3,19 +3,25 @@ using System.Collections;
 using Leap;
 using System.Collections.Generic;
 
-public class LeapPageTurnerV2 : MonoBehaviour {
+public class LeapPageTurnerV2 : MonoBehaviour
+{
 
     private MegaBookBuilder book;
     private AudioSource pageTurnSound;
     private Leap.Controller controller;
+    private int leftUpGestureProbability;
+    List<string> gestureHistory = new List<string>(); //´ToDO build own dataStructure ! 
 
-    public LeapPageTurnerV2(MegaBookBuilder book, Leap.Controller controller, AudioSource pageTurnSound) {
+    public LeapPageTurnerV2(MegaBookBuilder book, Leap.Controller controller, AudioSource pageTurnSound)
+    {
+        gestureHistory.Add("dummy");
         this.book = book;
         this.pageTurnSound = pageTurnSound;
         this.controller = controller;
     }
 
-    public void CheckPageTurnGesture(List<Hand> hands) {
+    public void CheckPageTurnGesture(List<Hand> hands)
+    {
         Hand rightHand = null;
         Hand leftHand = null;
 
@@ -31,9 +37,48 @@ public class LeapPageTurnerV2 : MonoBehaviour {
             }
         }
 
-        if (rightHand != null) {
-            Debug.Log(rightHand.PalmVelocity.x);
-            //righ
+        if (rightHand != null)
+        {
+            foreach (Hand newHand in controller.Frame(0).Hands)
+            {
+                if (newHand.IsRight)
+                {
+                    Vector currentHandPosition = newHand.PalmPosition;
+                    foreach (Hand oldHand in controller.Frame(1).Hands)
+                    {
+                        if (oldHand.IsRight)
+                        {
+                            Vector oldHandPosition = oldHand.PalmPosition;
+                            if (currentHandPosition.x < oldHandPosition.x && currentHandPosition.y > oldHandPosition.y)
+                            {
+                                leftUpGestureProbability++;
+                            }
+                            else {
+                                leftUpGestureProbability--;
+                            }
+                            if (leftUpGestureProbability < 0)
+                            {
+                                leftUpGestureProbability = 0;
+                            }
+                            if (leftUpGestureProbability > 15)
+                            {
+                                Debug.Log("Gesture leftUp detected");
+                                leftUpGestureProbability = 0;
+                                if (gestureHistory[0] != "leftUp")//Wenn das letzte nicht schon diese Geste war
+                                {
+                                    gestureHistory.Add("leftUp");
+                                    Debug.Log("first Item is " + gestureHistory[0] + " second Item is " + gestureHistory[1]);
+                                    Debug.Log("added to Que");
+                                }
+                            }
+                            
+                        }
+                    }
+                }
+            }
+            //Debug.Log(rightHand.PalmVelocity);
+
+            //for(int i)
         }
 
     }
